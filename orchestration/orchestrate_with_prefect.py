c7904130759769b8becbf3adbec6394f1d0a18e2
NicolasAyala2807 <nicolasayala2807@gmail.com>
2026-01-28 08:44:02 -0500

    advance

--- orchestration/orchestrate_with_prefect.py
+++ orchestration/orchestrate_with_prefect.py
@@ -25,7 +25,7 @@ from text_processing import TextProcessing
 warnings.filterwarnings("ignore")
 
 
-@task(retries=1, retry_delay_seconds=2,
+@task(retries=0, retry_delay_seconds=2,
       name=" Text processing task", 
       tags=["pos_tag"])
 def text_processing_task(language: str, file_name: str, version: int):
@@ -39,7 +39,7 @@ def text_processing_task(language: str, file_name: str, version: int):
     text_processing_processor = TextProcessing(language=language)
     text_processing_processor.run(file_name=file_name, version=version)
 
-@task(retries=1, retry_delay_seconds=2,
+@task(retries=0, retry_delay_seconds=2,
       name="Feature extraction task", 
       tags=["feature_extraction", "topic_modeling"])
 def feature_extraction_task(data_path_processed: str, 
@@ -54,7 +54,7 @@ def feature_extraction_task(data_path_processed: str,
     feature_extraction_processor.run(data_path_processed=data_path_processed, 
                                      data_version = VERSION)
 
-@task(retries=1, retry_delay_seconds=2,
+@task(retries=0, retry_delay_seconds=2,
       name="Data transformation task", 
       tags=["data_transform", "split_data", "train_test_split"])    
 def data_transformation_task_and_split(data_input_path: str, file_name: str, version: int):
@@ -84,7 +84,7 @@ def data_transformation_task_and_split(data_input_path: str, file_name: str, ver
     return X_train, X_test, y_train, y_test, count_vectorizer
 
 @task(
-    retries=1,
+    retries=0,
     retry_delay_seconds=2,
     name="Train best model",
     tags=["train", "best_model", "LogisticRegressionClassifier"],
@@ -97,11 +97,11 @@ def training_best_model(
     params: dict,
     model_name: str,
 ):
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("text_classification1")
+    
     with mlflow.start_run(run_name=model_name):
-
-        mlflow.set_tracking_uri("sqlite:///mlflow.db")
-        mlflow.set_experiment("text_classification1")
-
+    
         mlflow.set_tag("developer", DEVELOPER_NAME)
         mlflow.set_tag("model_name", MODEL_NAME)
         mlflow.log_params(params)
